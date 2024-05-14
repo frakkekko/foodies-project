98b430de49796a27ec6c7d73619bd79f5a183036
Francesco Sciadone <francescoscldn@gmail.com>
2024-05-14 02:17:56 +0200

    [FEATURE]: implemented cache refresh (revalidatePath) for production env

--- lib/actions.ts
+++ lib/actions.ts
@@ -5,6 +5,7 @@ import { redirect } from "next/navigation";
 import { deleteMeal, saveMeal } from "./meals";
 import { FormState, MealFormData } from "@/types/types";
 import { isValidShareMealInputForm } from "@/utils/utils";
+import { revalidatePath } from "next/cache";
 
 export async function shareMeal(prevState: FormState, formData: FormData): Promise<FormState> {
   const meal: MealFormData = {
@@ -23,10 +24,19 @@ export async function shareMeal(prevState: FormState, formData: FormData): Promi
   }
 
   await saveMeal(meal);
+  revalidatePath('/meals')
   redirect("/meals");
 }
 
 export async function deleteMealById(id: string, mealImagePath: string) {
   await deleteMeal(id, mealImagePath);
+  revalidatePath('/meals')
   redirect("/meals");
 }
+
+/*
+NOTA: revalidatePath viene utilizzato per fare il refresh della cache di NextJs
+  tuttavia le immagini che verranno inserite dall'utente non verranno caricate
+  (anche se viene fatto il refresh della cache), questo perché NextJs impone 
+  l'utilizzo di servizi esterni come AWS S3
+*/
